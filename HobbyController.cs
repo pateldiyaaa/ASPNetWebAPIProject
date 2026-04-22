a73578f4ac2c7e45d3b545802939ba86a5ed10f0
uhmaree <amarebrooks4@gmail.com>
2026-04-22 07:42:01 -0400

    Added 3 Controllers
    FoodController.cs, HobbyController.cs, MovieController.cs

---
+++ HobbyController.cs
@@ -0,0 +1,74 @@
+﻿using FinalProject.Interfaces;
+using FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HobbyController : ControllerBase
+    {
+        private readonly IHobbyDbContextDAO _hobbyDbContext;
+
+        public HobbyController(IHobbyDbContextDAO hobbyDbContext)
+        {
+            _hobbyDbContext = hobbyDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllRecords()
+        {
+            return Ok(_hobbyDbContext.GetAllRecords());
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetRecordById(int id)
+        {
+            var record = _hobbyDbContext.GetRecordById(id);
+
+            if (record == null)
+                return NotFound(id);
+
+            return Ok(record);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateRecord(Hobby hobby)
+        {
+            int? result = _hobbyDbContext.UpdateRecord(hobby);
+
+            if (result == null)
+                return NotFound(hobby);
+            if (result == 0)
+                return StatusCode(500, "Update failed. Please try again.");
+
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        public IActionResult DeleteRecord(int id)
+        {
+            int? result = _hobbyDbContext.RemoveRecord(id);
+
+            if (result == null)
+                return NotFound(id);
+            if (result == 0)
+                return StatusCode(500, "Delete failed. Please try again.");
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddRecord(Hobby hobby)
+        {
+            int? result = _hobbyDbContext.AddRecord(hobby);
+
+            if (result == null)
+                return StatusCode(500, "Add failed. Record already exists.");
+            if (result == 0)
+                return StatusCode(500, "Add failed. Please ensure the id field is set to 0, then try again.");
+
+            return Ok();
+        }
+    }
+}
---
+++ MovieController.cs
@@ -0,0 +1,74 @@
+﻿using FinalProject.Interfaces;
+using FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly IMovieDbContextDAO _movieDbContext;
+
+        public MovieController(IMovieDbContextDAO movieDbContext)
+        {
+            _movieDbContext = movieDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllRecords()
+        {
+            return Ok(_movieDbContext.GetAllRecords());
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetRecordById(int id)
+        {
+            var record = _movieDbContext.GetRecordById(id);
+
+            if (record == null)
+                return NotFound(id);
+
+            return Ok(record);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateRecord(Movie movie)
+        {
+            int? result = _movieDbContext.UpdateRecord(movie);
+
+            if (result == null)
+                return NotFound(movie);
+            if (result == 0)
+                return StatusCode(500, "Update failed. Please try again.");
+
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        public IActionResult DeleteRecord(int id)
+        {
+            int? result = _movieDbContext.RemoveRecord(id);
+
+            if (result == null)
+                return NotFound(id);
+            if (result == 0)
+                return StatusCode(500, "Delete failed. Please try again.");
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddRecord(Movie movie)
+        {
+            int? result = _movieDbContext.AddRecord(movie);
+
+            if (result == null)
+                return StatusCode(500, "Add failed. Record already exists.");
+            if (result == 0)
+                return StatusCode(500, "Add failed. Please ensure the id field is set to 0, then try again.");
+
+            return Ok();
+        }
+    }
+}
